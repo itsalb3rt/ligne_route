84d48960a33329a34295f022071c67889994cd00
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2018-12-08 17:05:56 -0400

    Mejora de la clase y funciones agregadas

--- src/ligne_route.js
+++ src/ligne_route.js
@@ -1,41 +1,33 @@
 /**
- * Utilitario para generar url hacia los controladores
- * de ligne framework
+ * Utilitario generar url relativas (Enfocado a AJAX)
  *
- * Uso básico;
- *
- * ruta normal
- * let route = new Route('foo','bar');
- *
- * ruta con parametros
- * let route = new Route('foo','bar',1);
- * O
- *let route = new Route('foo','bar','any');
- *
- * array de parametros
- * let route = new Route('foo','bar',['products',1,'any']);
- *
- * Para obtener la ruta solo debe invocarse el metodo route
- * console.log(route.route);
  **/
-
 
 class Route {
 
-    constructor(controller, action, params = null) {
+    /**
+     * Define las propiedades a utilizar
+     *
+     * @controller string
+     * @action string
+     * @params string | array | mixed
+     * @include_actual_pathname boolean | string
+     * Esta propiedad define si se dejara el path actual o se omitira, tambien
+     * permite enviar un path personalizado
+     **/
+    constructor(controller, action, params = null, include_actual_pathname = false) {
         //Obtiene la url actual
         this.url = new URL(window.location.href);
 
         this.host = this.url.host;
         this.protocol = this.url.protocol;
-        this.actualPathname = this.url.pathname;
+        this.actualPathname = this.custom_patchname(include_actual_pathname);
 
         this.controller = controller;
         this.action = action;
         this.parameters = params;
 
         this.newPathname = null;
-
         //Solo acepta caracteres alfabeticos
         this.regex = /[0-9éèêëùüàâöïç\"\/\%\(\).'?!,@$#-_ \n\r]/g;
         //Acepta caracteres alfanumericos
@@ -47,7 +39,14 @@ class Route {
             this.check_controller_action_is_valid();
             this.newPathname = this.patchName;
             this.parameters = (this.parameters != null) ? this.params : '';
-            return this.protocol + '//' + this.host + '/' + this.newPathname + this.parameters;
+
+            if (this.actualPathname == null) {
+                return this.protocol + '//' + this.host + '/' + this.newPathname + this.parameters;
+
+            } else {
+                return this.protocol + '//' + this.host + this.actualPathname + this.newPathname + this.parameters;
+
+            }
         } else {
             this.generate_new_throw('No se pueden generar rutas en un archivo local, debe tenerlo en un servidor.');
         }
@@ -108,5 +107,28 @@ class Route {
             params += '/' + this.parameters[index].toString().trim().replace(this.regex_alfanumeric, '');
         }
         return params;
+    }
+
+    /**
+     * Verifica que el ultimo caracter sea un slash
+     * de no ser asi se lo agregara para hacer que la
+     * ruta sea una ruta valida
+     **/
+    custom_patchname(path_indications) {
+        if (typeof path_indications == 'boolean') {
+
+            if (path_indications == false) {
+                if (this.url.pathname.slice(-1) != '/') {
+                    return this.url.pathname + '/';
+                } else {
+                    return this.url.pathname;
+                }
+            } else {
+                return null;
+            }
+        } else {
+            return '/' + path_indications + '/';
+        }
+
     }
 }
